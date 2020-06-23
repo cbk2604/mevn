6d31724e5154b972f564a4eb536033f8a7d2d76a
Bharghav <bharghav@happay.in>
2020-06-23 18:13:43 +0530

    add project

---
+++ client/src/components/CharacterCreater.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="character-creater">
+    <h1>Character Creater</h1>
+    <label for="character-name">Character Name: </label>
+    <input type="text" id="character-name" v-model="name" placeholder="Enter a name" /><br /><br />
+    <label for="professions-list">Character Profession: </label>
+    <select id="professions-list" v-model="profession">
+        <option value="Mage">Mage</option>
+        <option value="Thief">Thief</option>
+        <option value="Warrior">Warrior</option>
+    </select><br /><br />
+    <button v-on:click="post">Create Character</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'CharacterCreater',
+  data: function() {
+      return {
+          name: null,
+          profession: null
+      }
+  },
+  method: {
+    postCharacter: function() {
+      axios
+      .post('http://localhost:3000/characters', {
+        name: this.name,
+        profession: this.profession
+      });
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ client/src/components/CharacterViewer.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="character-viewer">
+    <h1>Character Viewer</h1>
+    <p v-for="{character, index} in characters" v-bind:key="index">{{character.name}} is a {{character.profession}}</p>
+    <button v-on:click="getCharaters">Refresh Characters</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'CharacterViewer',
+  data: function() {
+    return {
+      characters: null
+    }
+  },
+  methods: {
+    getCharaters: function() {
+      axios
+      .get('http://localhost:3000/characters')
+      .then(response => {this.characters = response.data})
+    }
+  },
+  mounted: function() {
+    this.getCharaters();
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
